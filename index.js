7b627963250eefdda11b7492f13559d97b8aa20e
hunghg255 <hunghg.agiletech@gmail.com>
2022-12-13 15:43:06 +0700

    update port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const upload = multer(); // for parsing multipart/form-data
 var cors = require('cors');
 
 const app = express();
-const port = 3005;
+const port = 5001;
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
